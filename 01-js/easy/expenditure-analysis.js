86f73987ed4f40fbb37920112bdce4f263f4570c
Vinayak <nayakvinayak95@gmail.com>
2023-06-12 22:04:22 +0530

    Solved expenditure analysis problem with tests

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,31 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+
+  var transaction_map = new Map();
+  // Iterate over each and every transaction and accumulate the total in a map 
+  transactions.forEach(element => {
+    let cat = element.category;
+    let price = element.price;
+
+    if (transaction_map.has(cat)){
+      val = transaction_map.get(cat)
+      transaction_map.set(cat, val + price);
+    }
+    else {
+      transaction_map.set(cat, price);
+    }
+  });
+
+  // Structure the output in the required manner and return that to the caller
+  let spends = [];
+  var counter = 0;
+  transaction_map.forEach((value, key) => {
+    spends[counter] = {"category": key, "totalSpent": value};
+    counter += 1;
+  })
+
+  return spends;
 }
 
 module.exports = calculateTotalSpentByCategory;
